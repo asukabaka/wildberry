43880fe23d7ecfae02c7d8a2ebc7ce572e3715f1
Raymond Lam <qu4ntumphysx@gmail.com>
2023-03-23 15:59:30 -0700

    test again

--- js/scroll-animation.js
+++ js/scroll-animation.js
@@ -1,9 +1,4 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // run whatever we need
-});
-
-function init() {    
-    const scrollElements = document.querySelectorAll(".js-scroll");
+const scrollElements = document.querySelectorAll(".js-scroll");
 
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
@@ -43,34 +38,3 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => { 
   handleScrollAnimation();
 });
-}
-
-
-const options = {
-  
-    plugins: [new SwupProgressPlugin(
-    {
-      className: 'swup-progress-bar',
-      transition: 800,
-      delay: 0
-    }
-  )]
-  
-
-};
-
-const swup = new Swup(options); // only this line when included with script tag
-
-// run once 
-init();
-
-// this event runs for every page view after initial load
-swup.on('contentReplaced', init);
-
-function unload() {
-    if (document.querySelector('.js-scroll')) {
-    }
-    // ...
-}
-
-swup.on('willReplaceContent', unload);
